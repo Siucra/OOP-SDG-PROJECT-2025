2737d9632d5ad72e0decce728197588df39be001
safiatdao <107083240+safiatdao@users.noreply.github.com>
2025-11-13 11:23:04 +0000

    branch adjustments
    Fixed an error where current branch files conflicted with main branch files.

--- FarmApp/src/farmapp/MainMenuGUI.java
+++ FarmApp/src/farmapp/MainMenuGUI.java
@@ -92,14 +92,11 @@ public class MainMenuGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void AnimalMBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AnimalMBtnActionPerformed
-        //redirects to a different interface
-        AnimalManagementGUI animalMGUI = new AnimalManagementGUI();
-        animalMGUI.setVisible(true);
-        this.dispose();
+
     }//GEN-LAST:event_AnimalMBtnActionPerformed
 
     private void salesInvBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salesInvBtnActionPerformed
-        // celina ---
+        
     }//GEN-LAST:event_salesInvBtnActionPerformed
 
     private void productTBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_productTBtnActionPerformed
